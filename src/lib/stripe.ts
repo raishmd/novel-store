cf2f9b61504f453a1f4fa8529648c4759e74e3e3
raishmd <rais7amidoo@gmail.com>
2026-05-14 13:19:02 +0100

    v 1.0.3

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,8 +1,22 @@
 import Stripe from "stripe"
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-02-24",
-  typescript: true,
+function createStripe() {
+  return new Stripe(process.env.STRIPE_SECRET_KEY!, {
+    apiVersion: "2025-02-24",
+    typescript: true,
+  })
+}
+
+let _stripe: Stripe | null = null
+export function getStripe() {
+  if (!_stripe) _stripe = createStripe()
+  return _stripe
+}
+
+export const stripe = new Proxy({} as Stripe, {
+  get(_, prop) {
+    return getStripe()[prop as keyof Stripe]
+  },
 })
 
 export function getStripePublishableKey() {
